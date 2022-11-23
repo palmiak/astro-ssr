9f36227862760a6e3ebc83abc64ca0788631c1ee
Maciek Palmowski <m.palmowski@freshpixels.pl>
2022-11-23 03:24:06 +0100

    port next try

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
@@ -12,6 +11,5 @@ export default defineConfig({
   output: 'server',
   adapter: node({
 	mode: 'standalone'
-  }),
-  server: { port: process.env.PORT }
+  })
 });
